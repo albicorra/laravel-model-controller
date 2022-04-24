dc3b26b3fd698874daad199694ea6950a4a4d668
Alberto Corrado <alberto2001corrado@gmail.com>
2022-04-24 16:09:04 +0200

    fine esercizio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,13 @@
     <div>
         <ul>
             @foreach ($movies as $movie)
-               <li>{{$movie->title}}</li> 
+               <li>
+                    <h2 class="title">{{$movie->title}}</h2>
+                    <div class="info">Original title: <span>{{$movie->original_title}}</span></div>
+                    <div class="info">Nationality: <span>{{$movie->nationality}}</span></div>
+                    <div class="info">Date: <span>{{$movie->date}}</span></div>
+                    <div class="info">Vote: <span>{{$movie->vote}}</span></div>
+                </li> 
             @endforeach
         </ul>
     </div>
